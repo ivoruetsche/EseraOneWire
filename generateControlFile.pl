3869475fb026ea47ef96d0520a1429dd230ceeea
pizmus <pizmus@turnbeutel-vergesser.com>
2018-10-02 22:54:29 +0200

    experimenting with update

--- generateControlFile.pl
+++ generateControlFile.pl
@@ -4,15 +4,18 @@ use File::Basename;
 use POSIX qw(strftime);
 use strict;
 
+
+
 my @filenames = ( "66_EseraOneWire.pm",
                   "66_EseraDigitalInOut.pm",
                   "66_EseraMulti.pm",
 		  "66_EseraTemp.pm");
 
+my $prefix = "FHEM";
 my $filename = "";
 foreach $filename (@filenames)
 {
-  my @statOutput = stat($filename);
+  my @statOutput = stat($prefix."/".$filename);
 
   my $mtime = $statOutput[9];
   my $date = POSIX::strftime("%Y-%d-%m", localtime($mtime));
@@ -21,5 +24,5 @@ foreach $filename (@filenames)
 
   my $filesize = $statOutput[7];
 
-  printf("UPD ".$filetime." ".$filesize." FHEM/".$filename."\n");
+  printf("UPD ".$filetime." ".$filesize." ".$prefix."/".$filename."\n");
 }
