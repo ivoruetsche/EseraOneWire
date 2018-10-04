56af36a7785d5a011fdc212b4e3a694eae4066e0
pizmus <pizmus@turnbeutel-vergesser.com>
2018-10-04 20:18:40 +0200

    move some documentation to README

--- FHEM/66_EseraOneWire.pm
+++ FHEM/66_EseraOneWire.pm
@@ -20,43 +20,6 @@
 # 
 ################################################################################
 #
-# Principles regarding use of the ESERA controller:
-# - Do not rely on previous state of the controller. Make all controller 
-#   settings that are needed for operation with this module in this module itself.
-# - Do not change the state of the controller persistently, e.g. do not use
-#   "save" commands to freeze settings or the list of connected 1-wire devices.
-# - Do not rely on ESERA IDs. ESERA IDs are basically indices into the list of 
-#   known devices maintained by the controller. Indices can change.
-# - For the same reason, hide ESERA IDs from the user. Present 1-wire IDs to
-#   the user. Map them to ESERA IDs internally only, and only when needed.
-# - Expect one response for each request sent to the controller. Exceptions:
-#   - Readings and events are sent asynchronously by the controller.
-#   - Setting digital outputs does not immediately cause a response packet. Responses
-#     cannot be distinguished from periodic readings.
-#   - KAL messages are sent asynchronously be the controller.
-#   Send only one request at a time. Wait for response before sending the next request.
-# - The controller has a watch. The time is ignored: Do not try to set the time, and
-#   do not use the time reported by the controller.
-# - The only reason to use the "Config Tool" from Esera is to update the controller
-#   firmware.
-#
-# Principles regarding communication with client modules:
-# - Readings are passed to clients without device specific interpretation. The format
-#   of the messages passed to clients is fixed. The message contains the 1-wire ID
-#   (as unique ID of the device), the device type (e.g. DS1820, so that the client
-#   module can do a consistency check), and the Esera ID (it can change with the next
-#   restart of the controller, but it is required to send messages from the client
-#   without interpretation in this module).
-# - If the client is defined to support an ESERA product number, but the controller
-#   has not been configured the the product number, it will report a "low level" 
-#   device type to this module. This results in a deviceType mismatch when interpreting
-#   a reading in the client. In that case the client will try to program the controller
-#   to use the ESERA product number, not the low level device type.
-# - A reading can be relevant to multiple devices, e.g. when the 8 digital inputs
-#   of a DS2408 are used with different clients, with non-overlapping bit ranges.
-#   There is no automatic check anywhere to detect overlapping bit ranges.
-# - Commands to client devices are described in the EseraOneWire_Write function.
-#
 # Data stored in device hash:
 # - Various information read from the controller e.g. during EseraOneWire_refreshControllerInfo().
 #   In most cases the hash key is named like the controller command.
